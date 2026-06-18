be37c8601ac7cafb3243815d079445e69ae28750
Ali Fetvacı <ali.fetvaci@getir.com>
2026-06-18 12:46:32 +0300

    commit

--- src/main/java/com/alifetvaci/walletapiapplication/repository/WalletCardRepository.java
+++ src/main/java/com/alifetvaci/walletapiapplication/repository/WalletCardRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 /**
- * PostgreSQL işlemleri
+ * PostgreSQL üzerinde WalletCard verisine erişim işlemleri.
  */
-public interface WalletCardRepository
-        extends JpaRepository<WalletCard, Long> {
+public interface WalletCardRepository extends JpaRepository<WalletCard, Long> {
 
+    /**
+     * Kart numarasına göre kart bilgisini getirir.
+     *
+     * SQL karşılığı:
+     * SELECT * FROM wallet_cards WHERE card_number = ?;
+     */
     Optional<WalletCard> findByCardNumber(String cardNumber);
 }
